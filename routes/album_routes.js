bcfa1830a64d6d6de65a36aced914f6079ce26d4
wayde <jaskela@sonoma.edu>
2016-12-14 01:31:01 -0800

    More major changes

--- routes/album_routes.js
+++ routes/album_routes.js
@@ -40,6 +40,7 @@ router.get('/', function(req, res){
 // View the tracks for the given album id
 router.get('/tracks', function(req, res){
     //console.log (req.query);
+    console.log (req);
     album_id = req.query['album_id'];
     if(req.query.album_id == null) {
         res.send('album_id is null');
